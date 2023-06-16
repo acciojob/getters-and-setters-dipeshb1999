9a8d0e5ea31202a2dc1389edea62c765bd79e71a
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-06-16 17:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(value) {
+    this._age = value;
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
+
+// Create instances and call methods
+const person = new Person("John", 25);
+const student = new Student("Alice", 20);
+const teacher = new Teacher("Mr. Smith", 40);
+
+person.age = 30;
+student.study();
+teacher.teach();
+
+// Display output
+const outputDiv = document.getElementById('output');
+outputDiv.innerHTML = `
+  <p>Person:</p>
+  <p>Name: ${person.name}</p>
+  <p>Age: ${person.age}</p>
+  <br>
+  <p>Student:</p>
+  <p>Name: ${student.name}</p>
+  <p>Age: ${student.age}</p>
+  <br>
+  <p>Teacher:</p>
+  <p>Name: ${teacher.name}</p>
+  <p>Age: ${teacher.age}</p>
+`;
